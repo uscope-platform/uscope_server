3cfcc35baeea7657fb626f6d29b63c3c4083cf5b
Filippo Savi <filippo.savi@nottignham.edu.cn>
2019-08-19 16:29:30 +0200

    Added timebase parsing and enable counter value calculation

--- uCube_interface/uCube_interface.py
+++ uCube_interface/uCube_interface.py
@@ -30,6 +30,9 @@ class uCube_interface:
         filename = ctypes.c_char_p(driver_file.encode("utf-8"))
         self.low_level_lib.low_level_init(filename, self.buffer_size, 0x7E200000, 0x43c00000)
 
+        self.clock_frequency = 100e6
+
+
     def wait_for_data(self):
         return self.low_level_lib.wait_for_Interrupt()
 
@@ -40,8 +43,23 @@ class uCube_interface:
         rec_data = [arr[i] for i in range(1024)]
         return rec_data[::-1]
 
-
-    def change_timebase(self, timebase):
+    def change_timebase(self,str_timebase : str):
+        timebase = 0
+        str_timebase = str_timebase.replace(" ", "")
+        if str_timebase[-1].isdigit():
+            timebase = float(str_timebase)
+        else:
+            if str_timebase[-1] == 'm':
+                timebase = float(str_timebase[0:-1])*1e-3
+            elif str_timebase[-1] == 'u' or timebase[-1] == 'U':
+                timebase = float(str_timebase[0:-1])*1e-6
+            elif str_timebase[-1] == 'n' or timebase[-1] == 'N':
+                timebase = float(str_timebase[0:-1]) * 1e-9
+            elif str_timebase[-1] == 'K' or timebase[-1] == 'K':
+                timebase = float(str_timebase[0:-1]) * 1e3
+            elif str_timebase[-1] == 'M':
+                timebase = float(str_timebase[0:-1]) * 1e6
+        counter_val = round(timebase/self.clock_frequency**-1)
         pass
 
 if __name__ == '__main__':
