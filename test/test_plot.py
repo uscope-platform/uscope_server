352fe53f665654908e7b01783e07ecaa8140413b
filippo savi <filssavi@gmail.com>
2019-09-14 17:48:09 +0200

    started working on unit testing for better developability

---
+++ test/test_plot.py
@@ -0,0 +1,21 @@
+import os
+import tempfile
+
+import pytest
+
+import app_factory
+
+
+@pytest.fixture
+def client():
+        app = app_factory.create_app(debug=True)
+        with app.test_client() as client:
+            yield client
+
+def test_plotChannels(client):
+    # pre-test setup, select application
+    response = client.get('/application/specs/AdcTest')
+    # test proper
+    known_good_response = b'[{"enabled":false,"id":1,"max_value":180,"min_value":0,"name":"Current A"},{"enabled":false,"id":2,"max_value":180,"min_value":0,"name":"Current B"},{"enabled":false,"id":3,"max_value":180,"min_value":0,"name":"Current C"},{"enabled":false,"id":4,"max_value":600,"min_value":0,"name":"Voltage A"},{"enabled":false,"id":5,"max_value":600,"min_value":0,"name":"Voltage B"},{"enabled":false,"id":6,"max_value":600,"min_value":0,"name":"Voltage C"}]\n'
+    response = client.get('/plot/channels/specs')
+    assert known_good_response in response.data
