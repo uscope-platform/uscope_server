781d01b865ca70c7a511f00e9793731b09358b62
filssavi <filippo.savi@nottingham.edu.cn>
2021-03-18 22:40:55 +0100

    Started splitting data store in multiple classes with sqalchemy orm implementation

--- Store/data_store/Applications.py
+++ Store/data_store/Applications.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects import postgresql
-
-from sqlalchemy import create_engine
+from sqlalchemy import select
 
 # declarative base class
 Base = declarative_base()
@@ -26,3 +25,64 @@ class Applications(Base):
     def __repr__(self):
         return "<Application(application_name='%s', bitstream='%s')>" % (
                              self.application_name, self.bitstream)
+
+
+class ApplicationsStore:
+
+    def __init__(self, sesssion):
+        self.Session = sesssion
+
+
+    def application_from_row(self, row : Applications):
+        app = {}
+        app['application_name'] = row.application_name
+        app['bitstream'] = row.bitstream
+        app['clock_frequency'] = row.clock_frequency
+        app['channels'] = row.channels
+        app['channel_groups'] = row.channel_groups
+        app['initial_registers_values'] = row.initial_registers_values
+        app['macro'] = row.macro
+        app['parameters'] = row.parameters
+        app['peripherals'] = row.peripherals
+        for i in row.miscellaneous:
+            app[i] = row.miscellaneous[i]
+        return app
+
+    def add_application(self, app):
+        item = Applications(application_name=app["application_name"], bitstream=app['bitstream'],
+                            clock_frequency=app['clock_frequency'], channels=app['channels'],
+                            channel_groups=app['channel_groups'],
+                            initial_registers_values=app['initial_registers_values'], macro=app['macro'],
+                            parameters=app['parameters'], peripherals = app['peripherals']
+                            )
+        with self.Session() as session:
+            with session.begin():
+                session.add(item)
+
+    def get_applications_dict(self):
+        with self.Session() as session:
+            with session.begin():
+                result = session.query(Applications).all()
+                apps = {}
+                for row in result:
+                    apps[row.application_name] = self.application_from_row(row)
+        return apps
+
+    def get_application(self, name):
+        try:
+            with self.Session() as session:
+                result = session.query(Applications).filter_by(application_name=name).first()
+            return self.application_from_row(result)
+        except AttributeError:
+            raise KeyError('Application Not Found')
+
+    def remove_application(self, name):
+        try:
+            with self.Session() as session:
+                application = session.query(Applications).filter_by(application_name=name).first()
+        except AttributeError:
+            raise KeyError('Application Not Found')
+
+        with self.Session() as session:
+            with session.begin():
+                session.delete(application)
--- Store/data_store/__init__.py
+++ Store/data_store/__init__.py
@@ -1,21 +1,65 @@
-from sqlalchemy import Column, String
+
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.dialects import postgresql
 
 from sqlalchemy import create_engine
 
 import Applications
+import Scripts
 
 # declarative base class
 Base = declarative_base()
 
 
-engine = create_engine("postgresql+psycopg2://uscope:test@172.23.0.4/uscope")
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+class DataStore:
 
-a = 0
+    def __init__(self):
+        self.engine = create_engine("postgresql+psycopg2://uscope:test@172.23.0.4/uscope")
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
 
-for instance in session.query(Applications.Applications):
-    print(instance)
+        self.app_store = Applications.ApplicationsStore(self.Session)
+        self.script_store = Scripts.ScriptsStore(self.Session)
+
+    def add_application(self, app):
+        self.app_store.add_application(app)
+
+    def get_applications_dict(self):
+        return self.app_store.get_applications_dict()
+
+    def get_application(self, name):
+        return self.app_store.get_application(name)
+
+    def remove_application(self, name):
+        self.app_store.remove_application(name)
+
+    def get_script(self, id):
+        return self.script_store.get_script(id)
+
+    def get_scripts_dict(self):
+        return self.script_store.get_scripts_dict()
+
+    def add_scripts(self, script):
+        self.script_store.add_scripts(script)
+
+    def remove_scripts(self, script):
+        self.script_store.remove_scripts(script)
+
+
+store = DataStore()
+
+result = store.get_applications_dict()
+result1 = store.get_application('SicDrive')
+
+app = {'application_name': "test", 'bitstream': "test.bin", 'clock_frequency': 10000, 'channels': [],
+       'channel_groups': [], 'initial_registers_values': [], 'macro': [], 'parameters': [], 'peripherals': []}
+
+store.add_application(app)
+store.remove_application("test")
+
+scripts = store.get_scripts_dict()
+script = store.get_script(25)
+
+scr = {'id': 100, 'name': "test", 'path': 'test.sh', 'script_content': 'asfgfweg', 'triggers': 'test trigger'}
+store.add_scripts(scr)
+store.remove_scripts(100)
+pass
