15f76b3409aa6d376adf95b5b897537d8f368f32
Filippo Savi <filisavi@unimore.it>
2021-07-13 13:46:09 +0200

    updated compiler handling for new version
    the new version of the compiler is fully standalone, as continuing the integration in python as an externally  bound module is cumbersome. 
    
    The relevant compiler is invoked through the subprocess module and json is used to exchange output info

--- fCore_compiler/fCore_compiler.py
+++ fCore_compiler/fCore_compiler.py
@@ -1,14 +1,40 @@
-import fCore_has_py as has
+import json
+import os
+import subprocess
 
 class CompilerBridge:
 
     def compile(self, file_content: str):
-        try:
-            program_content, program_size = has.fCore_has_embeddable_s(file_content)
-            return program_content, program_size
-        except RuntimeError as error:
-            raise ValueError(str(error))
+
+        fCore_has_input = '/tmp/fCore_has_in.s'
+        fCore_has_output = '/tmp/output.json'
+
+        text_file = open(fCore_has_input, 'w')
+        n = text_file.write(file_content)
+        text_file.close()
 
 
+        subprocess.run(['fCore_has', '--json', '--o', fCore_has_output, fCore_has_input])
+
+        with open(fCore_has_output) as json_file:
+            out = json.load(json_file)
+
+        if 'error_code' not in out:
+            raise ValueError('INTERNAL ERROR: The fCore_cc compiler returned a malformed message')
+
+        if out['error_code'] != '':
+            raise ValueError(out['error_code'])
+
+        if 'program_size' in out:
+            program_size = out['program_size']
+        else:
+            program_size = -1
+
+        os.remove(fCore_has_input)
+        os.remove(fCore_has_output)
+        return out['compiled_program'], program_size
 
 
+if __name__ == '__main__':
+    cb = CompilerBridge()
+    print(cb.compile("add r1, r1, r1"))
