4ed09ad0d8ad1b382110e40fd136ce9808817aa6
Filippo Savi <filisavi@unimore.it>
2021-08-08 16:24:02 +0200

    Implemented bitstream files dump and restore

--- uScopeBackend/db_manager.py
+++ uScopeBackend/db_manager.py
@@ -2,6 +2,7 @@ from flask import current_app, Blueprint, Response, request
 from flask_restful import Api, Resource
 from flask_jwt_extended import jwt_required
 import json
+import os
 
 from . import role_required
 
@@ -52,7 +53,27 @@ class DatabaseManager:
 
     def db_export(self):
         dump = self.store.dump()
+        dump['bitstream_contents'] = self.export_bitstreams()
         return dump
 
     def db_import(self, database):
+        self.restore_bitstreams(database['bitstream_contents'])
         self.store.restore(database)
+
+    def export_bitstreams(self):
+        bitstreams_dict = {}
+        debug_config = os.environ.get("DEBUG")
+        if debug_config == "TRUE":
+            bitstreams_dict = self.store.Elements.get_bitstreams_dict()
+            bitstreams_dump = {}
+            for i in bitstreams_dict:
+                path = bitstreams_dict[i]['path']
+                with open(path, mode='rb') as file:
+                    bitstreams_dump[path] = file.read()
+
+        return bitstreams_dict
+
+    def restore_bitstreams(self, dump):
+        for i in dump:
+            with open(i, mode='wb') as file:
+                file.write(dump[i])
