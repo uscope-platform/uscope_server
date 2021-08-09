75c06da62b2c931a1f6f24599e699e97440cd595
Filippo Savi <filisavi@unimore.it>
2021-08-09 16:36:01 +0200

    wait for pg database to be available at startup to avoid docker container crashing the first time the stack is brought up

--- Store/__init__.py
+++ Store/__init__.py
@@ -1,9 +1,9 @@
 from .ElementDataStore import ElementsDataStore
 from .AuthStore import AuthStore
 from .SettingsStore import SettingsStore
-
+from sqlalchemy.exc import OperationalError
 import os
-
+import time
 class Store:
     def __init__(self, clear_settings=True, update_ude_versions_on_init=True):
 
@@ -15,10 +15,23 @@ class Store:
             redis_host="redis"
             pg_host="postgresql+psycopg2://uscope:test@database/uscope"
 
-        self.Settings = SettingsStore(clear_settings=clear_settings, host=redis_host)
 
-        self.Auth = AuthStore(pg_host)
-        self.Elements = ElementsDataStore(update_ude_versions_on_init=update_ude_versions_on_init,host=pg_host)
+        self.Settings = SettingsStore(clear_settings=clear_settings, host=redis_host)
+        pg_available = False
+        pg_start_tries_left = 150
+        while not pg_available:
+            try:
+                self.Auth = AuthStore(pg_host)
+                self.Elements = ElementsDataStore(update_ude_versions_on_init=update_ude_versions_on_init,host=pg_host)
+            except OperationalError:
+                if pg_start_tries_left == 0:
+                    raise RuntimeError("ERROR: Could not connect to the postgres database")
+                pg_start_tries_left -= 1
+                time.sleep(0.1)
+            else:
+                pg_available = True
+
+
 
     def dump(self):
         dump = {'auth': self.Auth.dump(), 'elements': self.Elements.dump(), 'settings': self.Settings.dump()}
