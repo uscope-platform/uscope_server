2fd497dfd0dae196f822388c1d94bf679a4ae3cd
Filippo Savi <filisavi@unimore.it>
2021-09-17 11:53:06 +0200

    Added author

--- app_factory.py
+++ app_factory.py
@@ -1,4 +1,5 @@
 # Copyright 2021 University of Nottingham Ningbo China
+# Author: Filippo Savi <filssavi@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
--- gunicorn.py
+++ gunicorn.py
@@ -1,4 +1,5 @@
 # Copyright 2021 University of Nottingham Ningbo China
+# Author: Filippo Savi <filssavi@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 bind = "0.0.0.0:8989"
 workers = 2
 loglevel = "error"
